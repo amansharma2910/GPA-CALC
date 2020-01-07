833da016ea547286fbb3bcea3d54cc5dea810e64
neel229 <device.neel@gmail.com>
2020-01-07 15:03:08 +0530

    Go version

--- go-version.go
+++ go-version.go
@@ -1,5 +1,47 @@
 package main
 
+import "fmt"
+var gpa float64
+
 func main() {
-	$END$
+	// Schema
+	var totalCredit float64
+	var totalSubjects int
+	var letterGrade string
+	var credit float64
+	var score float64
+	var totalScore float64
+	// Takes value of credits and subjects from user
+	fmt.Print("Enter total credits")
+	_, _ =  fmt.Scan(&totalCredit)
+	fmt.Print("Enter total number of subjects")
+	_, _ =  fmt.Scan(&totalSubjects)
+	n := totalSubjects
+
+
+	for i:=0; i < n; i++ {
+		fmt.Println("Enter letterGrade")
+		_, _ = fmt.Scanf("%s", &letterGrade)
+		fmt.Println("Enter credits")
+		_, _ = fmt.Scan(&credit)
+		if letterGrade == "S" || letterGrade == "s" {
+			score = credit * 10
+		} else if letterGrade == "A" || letterGrade == "a" {
+			score = credit * 9
+		} else if letterGrade == "B" || letterGrade == "b" {
+			score = credit * 8
+		} else if letterGrade == "C" || letterGrade == "c" {
+			score = credit * 7
+		} else if letterGrade == "D" || letterGrade == "d" {
+			score = credit * 6
+		} else if letterGrade == "E" || letterGrade == "e" {
+			score = credit * 5
+		} else if letterGrade == "F" || letterGrade == "f" {
+			score = credit * 0
+		}
+		totalScore += score
+	}
+	gpa = totalScore / totalCredit
+	fmt.Println(gpa)
 }
+
